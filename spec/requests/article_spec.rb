e221efa97e47b01bdce7ce25cddfd36f09efe40d
unknown <ibr.s.ahmat@gmail.com>
2022-12-04 01:24:27 +0300

    Add tests for SearchArticle

---
+++ spec/requests/article_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Articles", type: :request do
+  describe "- test for Articles requests" do
+    before(:all) do
+      @article = Article.create(title: "Article 1", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+      Article.create(title: "Article 2", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+    end
+
+    it 'articles#index - should response status success' do
+      get articles_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'articles#show - should response status success' do
+      get article_path(@article.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'articles#create - should response status create or (200)' do
+      get new_article_path
+      Article.create(title: "Article 3", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+      expect(response).to have_http_status(200)
+    end
+  end
+end
