5ff54a653096bb0084544b14cc30dcf21d7b955e
unknown <ibr.s.ahmat@gmail.com>
2022-12-03 22:56:34 +0300

    Create seed file, and add RSpec configurations

--- db/migrate/20221203092614_create_articles.rb
+++ db/migrate/20221203092614_create_articles.rb
@@ -3,8 +3,8 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :body
+      t.string "title", null: false
+      t.text "body", null: false
 
       t.timestamps
     end
--- db/migrate/20221203161500_create_search_articles.rb
+++ db/migrate/20221203161500_create_search_articles.rb
@@ -3,7 +3,7 @@
 class CreateSearchArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :search_articles do |t|
-      t.string :title
+      t.string "title", null: false
 
       t.timestamps
     end
